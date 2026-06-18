aea8417cbacd42c1acf53f53812f11b39dbe456e
Murilo <oxelot17898@gmail.com>
2026-06-18 13:33:22 -0300

    arrumando a estruturação de pastas

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -355,7 +355,7 @@ async function init() {
 function verificar401(res) {
   if (res.status == 401) {
     localStorage.removeItem('token')
-    window.location.href = 'index.html'
+    window.location.href = '../../index.html'
     throw new Error('Não autorizado')
   }
 }
